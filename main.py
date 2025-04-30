01a807281964015b869c99558aae033134fe9997
Igor-Mozharov <r5@ukr.net>
2025-04-30 14:45:41 +0300

    add6

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from telegram_bot.telegram_main import start, button, handle_file, dialog_handler
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from credentials import credentialss
+import threading
+from flask import Flask
+import os
+
+
+def start_web_server():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return 'Bot is running (polling)...'
+
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
+    threading.Thread(target=start_web_server).start()
+
     application = ApplicationBuilder().token(credentialss('BOT_TOKEN')).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CallbackQueryHandler(button))
