53ae6f611be16e73e927059c1144268c2f54f368
Igor-Mozharov <r5@ukr.net>
2025-05-07 15:25:49 +0300

    add10

--- openai_api/openai_main.py
+++ openai_api/openai_main.py
@@ -7,7 +7,7 @@ def gemini_speaker(text):
     :param text: USer text
     :return Gemini response text
     """
-    genai.configure(api_key='AIzaSyBluTIHu_s-0PkrMtpLKm3bsJsFSxOmC9s')
+    genai.configure(api_key='AIzaSyDCVlZCAR2e0sgFbDogUa2YeFaO39-MXUI')
     model = genai.GenerativeModel('models/gemini-2.0-flash')
     response = model.generate_content(text)
     return response.text
